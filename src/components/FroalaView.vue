311e7abcf344e684706657252c40e32ec40e5419
zezhou.li <zezhou.li@epichust.com>
2024-10-16 16:59:12 +0800

    init

---
+++ src/components/FroalaView.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  props: ['tag', 'value'],
+
+  watch: {
+    value: function (newValue) {
+      this._element.innerHTML = newValue
+    },
+  },
+
+  created: function () {
+    this.currentTag = this.tag || this.currentTag
+  },
+
+  render: function (h) {
+    return h(this.currentTag, {
+      class: 'fr-view',
+    })
+  },
+
+  // After first time render.
+  mounted: function () {
+    this._element = this.$el
+
+    if (this.value) {
+      this._element.innerHTML = this.value
+    }
+  },
+
+  data() {
+    return {
+      currentTag: 'div',
+      _element: null,
+    }
+  },
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './style/index.css';
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+import Vue from 'vue';
+import App from './App.vue';
+
+new Vue({
+    render: (h) => h(App),
+}).$mount('#app');
