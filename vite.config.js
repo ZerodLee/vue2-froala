ec200fec7309975c5e4a589d5237f6beb5997e17
ZerodLee <eolo3@qq.com>
2024-10-17 16:47:48 +0800

    optim

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,16 @@ export default defineConfig({
       name: 'vue2Froala',
       fileName: 'vue2-froala',
     },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
     // sourcemap: true,  // 启用源映射
     // rollupOptions: {
     //     input: ["./src/component/index.js"],
